0df002a13a247ad25143c3891de3e598750c1fdc
Liting Huang <lh3119@columbia.edu>
2022-10-03 17:47:32 +0000

    part1 started

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.with_ratings
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = [] 
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+	def self.all_rating
+		['G', 'PG', 'PG-13', 'R']
+	end
+
+	def self.with_ratings(ratings_list)
+		if ratings_list.nil? 
+			where(rating: ratings_list) 
+		else
+			all
+		end
+	end
+
+
 end
